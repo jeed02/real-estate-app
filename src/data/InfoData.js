445586f5bb4f4f56558673c200b3074d1e703b2d
JD Briones <jdbriones22@gmail.com>
2021-07-30 20:57:55 +1200

    full app

---
+++ src/data/InfoData.js
@@ -0,0 +1,21 @@
+import ImageOne from "../images/house.jpg";
+
+export const InfoData = {
+  heading: "Explore our beautiful homes",
+  paragraphOne: "kdsjhfksdf",
+  paragraphTwo: "dhfousdhfiuhsdf",
+  buttonLabel: "View Homes",
+  image: ImageOne,
+  reverse: false,
+  delay: 100,
+};
+
+export const InfoDataTwo = {
+  heading: "I wanna die",
+  paragraphOne: "kdsjhsfdasfsdfsdfksdf",
+  paragraphTwo: "dhfousdhfiuhasdasdadsdasdsdf",
+  buttonLabel: "View Homes",
+  image: ImageOne,
+  reverse: true,
+  delay: 300,
+};
---
+++ src/data/SliderData.js
@@ -0,0 +1,36 @@
+import ImageOne from "../images/house.jpg";
+
+export const SliderData = [
+  {
+    title: "House 1",
+    price: "$4,280,000",
+    path: "/homes",
+    label: "View Home",
+    image: ImageOne,
+    alt: "House",
+  },
+  {
+    title: "House 2",
+    price: "$4,280,000",
+    path: "/homes",
+    label: "View Home",
+    image: ImageOne,
+    alt: "House",
+  },
+  {
+    title: "House 3",
+    price: "$4,280,000",
+    path: "/homes",
+    label: "View Home",
+    image: ImageOne,
+    alt: "House",
+  },
+  {
+    title: "Luxury Villa in Bali, Indonesia",
+    price: "$4,280,000",
+    path: "/homes",
+    label: "View Home",
+    image: ImageOne,
+    alt: "House",
+  },
+];
